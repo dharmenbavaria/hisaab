53ea740b48b260ebae70cd2810bbc81671c569a8
vasav-designer <v.a.ajudiya95@gmail.com>
2022-11-25 00:12:49 +0530

    QA page design structure ready.

--- html/js/custom.js
+++ html/js/custom.js
@@ -59,5 +59,57 @@ jQuery(document).ready(function(){
 
         jQuery(this).addClass('current');
         jQuery("#"+tab_id).addClass('current');
-    })
+    });
 });
+
+//test for getting url value from attr
+// var img1 = $('.test').attr("data-thumbnail");
+// console.log(img1);
+
+//test for iterating over child elements
+var langArray = [];
+$('.vodiapicker option').each(function() {
+    var img = $(this).attr("data-thumbnail");
+    var text = this.innerText;
+    var value = $(this).val();
+    var item = '<li><img src="' + img + '" alt="" value="' + value + '"/><span>' + text + '</span></li>';
+    langArray.push(item);
+})
+
+$('#lang-a').html(langArray);
+
+//Set the button value to the first el of the array
+$('.lang-btn-select').html(langArray[0]);
+$('.lang-btn-select').attr('value', 'en');
+
+//change button stuff on click
+$('#lang-a li').click(function() {
+    var img = $(this).find('img').attr("src");
+    var value = $(this).find('img').attr('value');
+    var text = this.innerText;
+    var item = '<li><img src="' + img + '" alt="" /><span>' + text + '</span></li>';
+    $('.lang-btn-select').html(item);
+    $('.lang-btn-select').attr('value', value);
+    $(".lang-b").toggle();
+    $("body").removeClass('open-select-box');
+    //console.log(value);
+});
+
+$(".lang-btn-select").click(function() {
+    $("body").addClass('open-select-box');
+    $(".lang-b").fadeToggle();
+});
+
+//check local storage for the lang
+var sessionLang = localStorage.getItem('lang');
+if (sessionLang) {
+    //find an item with value of sessionLang
+    var langIndex = langArray.indexOf(sessionLang);
+    $('.lang-btn-select').html(langArray[langIndex]);
+    $('.lang-btn-select').attr('value', sessionLang);
+} else {
+    var langIndex = langArray.indexOf('ch');
+    console.log(langIndex);
+    $('.lang-btn-select').html(langArray[langIndex]);
+    //$('.btn-select').attr('value', 'en');
+}
